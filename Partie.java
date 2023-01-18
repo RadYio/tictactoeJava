f00b558f08939a3f31a476f432816b6134ff8fe2
Hadamar en Personne <allan.gonin-saget.etu@univ-lemans.fr>
2023-01-18 16:34:03 +0100

    ushing all

---
+++ Partie.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Partie{
+    ArrayList<Case> listeDeCases;
+    Joueur joueur1;
+    Joueur joueur2;
+
+
+    Partie(){
+        listeDeCases = new ArrayList<Case>();
+    }
+}
---
+++ Reverse.java
@@ -0,0 +1,31 @@
+import java.rmi.*;
+import java.rmi.server.*;
+public class Reverse extends UnicastRemoteObject implements ReverseInterface{ 
+
+    public Reverse() throws RemoteException {
+        super();
+    }
+
+    public String reverseString(String chaineOrigine) throws RemoteException{
+
+        int longueur = chaineOrigine.length();
+        StringBuffer temp=new StringBuffer(longueur);
+
+        for (int i=longueur; i>0; i--)
+            temp.append(chaineOrigine.substring(i-1, i));
+
+        return temp.toString();
+    }
+
+    public String reverseStringCustom(String chaineOrigine) throws RemoteException{
+
+        int longueur = chaineOrigine.length();
+        StringBuffer temp=new StringBuffer(longueur);
+
+        for (int i=longueur; i>0; i--)
+            temp.append(chaineOrigine.substring(i-1, i));
+
+        System.out.println(temp.toString());
+        return "Fesse".toString();
+    }
+}
