b7c1f37940649343316a760484ba38f6d10b00e6
Allan GONIN-SAGET <allan.gonin-saget.etu@univ-lemans.fr>
2023-01-23 23:28:30 +0100

    chat est un panel maintenant bravo

--- Chat.java
+++ Chat.java
@@ -5,8 +5,49 @@ import java.io.File;
 import java.awt.*;
 
 public class Chat extends JPanel{
-    
+    static int largeur = 400;
+    static int hauteur = 500;
     Chat(){
+        super();
+        Box boiteTotale = new Box(BoxLayout.Y_AXIS); 
+
+        //gestion de la zone de texte des messages deja envoyés
+        JTextArea zoneChat = new JTextArea();
+        zoneChat.setEditable(false);
+        zoneChat.setPreferredSize(new Dimension(largeur, hauteur - 100));
+        zoneChat.setBackground(Color.LIGHT_GRAY);
+
+        //On ajoute cette zoneChat a la boiteTotale
+        boiteTotale.add(zoneChat);
+
+
+        Box boiteMessageEtBouton = new Box(BoxLayout.X_AXIS);
+
+        //gestion de la zone de texte pour envoyer un message et du bouton d'envoi du message à droite de la zone de texte
+        JTextField messageAEnvoyer = new JTextField("Message à envoyer");
+        messageAEnvoyer.setPreferredSize(new Dimension(largeur - 100, hauteur - 400));
+
+    
+        //Petit trick pour avoir une image sur un bouton de la bonne taille
+        ImageIcon iconeDeBonneTaille = new ImageIcon();
+        try{
+            Image img = ImageIO.read(new File("./assets/icon_send.png"));
+            iconeDeBonneTaille.setImage(img.getScaledInstance(100, hauteur - 400, java.awt.Image.SCALE_SMOOTH));
+        }catch(Exception e){
+            System.out.println("Erreur");
+        }
+
+        JButton boutonEnvoyer = new JButton(iconeDeBonneTaille);
+        boutonEnvoyer.setPreferredSize(new Dimension(largeur - 300, hauteur - 400));
+
+        //On ajoute le messageAEnvoyer et le bouton a la boiteMessageEtBouton
+        boiteMessageEtBouton.add(messageAEnvoyer);
+        boiteMessageEtBouton.add(boutonEnvoyer);
+
+        //On ajoute la boiteMessageEtBouton a la boiteTotale
+        boiteTotale.add(boiteMessageEtBouton);
+
+        this.add(boiteTotale);
 
     }
 
@@ -15,84 +56,16 @@ public class Chat extends JPanel{
         //gestion de la fenetre
         JFrame fenetre = new JFrame("Chat");
         fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        fenetre.setSize(300, 500);
+        fenetre.setSize(largeur, hauteur);
         fenetre.setLocationRelativeTo(null);
         fenetre.setResizable(false);
 
-
-        Box b1 = new Box(BoxLayout.Y_AXIS);
-
-        //gestion de la zone de texte des messages deja envoyés
-        JTextArea zoneChat = new JTextArea();
-        zoneChat.setEditable(false);
-        zoneChat.setPreferredSize(new Dimension(300,400));
-
-        //changer la couleur de fond en gris(x)
-        zoneChat.setBackground(Color.LIGHT_GRAY);
-        b1.add(zoneChat);
-        Box b2 = new Box(BoxLayout.X_AXIS);
-
-        //gestion de la zone de texte pour envoyer un message et du bouton d'envoi du message à droite de la zone de texte
-        JTextField messageAEnvoyer = new JTextField("Message à envoyer");
-        messageAEnvoyer.setPreferredSize(new Dimension(200,100));
-        b2.add(messageAEnvoyer);
-
+        Chat notreChat = new Chat();
         
-        //Petit trick pour avoir une image sur un bouton de la bonne taille
-        ImageIcon iconeDeBonneTaille = new ImageIcon();
-        try{
-            Image img = ImageIO.read(new File("./assets/icon_send.png"));
-            iconeDeBonneTaille.setImage(img.getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH));
-        }catch(Exception e){
-            System.out.println("Erreur");
-        }
-        
-        JButton boutonEnvoyer = new JButton(iconeDeBonneTaille);
-        
-        boutonEnvoyer.setPreferredSize(new Dimension(100, 100));
-        b2.add(boutonEnvoyer);
-
-        b1.add(b2);
-        //gestion du panel qui contiendra le chat et son bouton
-        JPanel panel = new JPanel();
-        panel.add(b1);
-        fenetre.add(b1);
+        fenetre.add(notreChat);
         fenetre.pack();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /* 
         
-
-        //gestion de la zone de texte des messages deja envoyés
-        JTextArea zoneChat = new JTextArea();
-        zoneChat.setEditable(false);
-        //changer la couleur de fond en gris(x)
-        zoneChat.setBackground(Color.LIGHT_GRAY);
-        panel.add(zoneChat);
-
-        //gestion de la zone de texte pour envoyer un message et du bouton d'envoi du message à droite de la zone de texte
-        JTextField messageAEnvoyer = new JTextField("Message à envoyer");
-        panel.add(messageAEnvoyer);
-        JButton boutonEnvoyer = new JButton("Envoyer");
-        panel.add(boutonEnvoyer);
-        */
-        
-
-        
+    
         //Fenetre visible
         fenetre.setVisible(true);
     }
