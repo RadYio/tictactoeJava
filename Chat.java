01706f12ef598f759a9319b0815fabd46ae62319
Allan GONIN-SAGET <allan.gonin-saget.etu@univ-lemans.fr>
2023-01-23 21:16:54 +0100

    tout propre pour  faire un chat

---
+++ Chat.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class Chat extends JPanel{
+    
+    Chat(){
+        setLayout(null);
+    }
+
+    public static void main(String[] args){
+        new Chat();
+    }
+}
