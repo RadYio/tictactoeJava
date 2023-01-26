b3cc827015abc5e089117b14c592d8622c1bd5c5
Hadamar en Personne <allan.gonin-saget.etu@univ-lemans.fr>
2023-01-26 13:30:32 +0100

    j'espere qu'avec ca ca sera mieux

--- AffichageChat.java
+++ AffichageChat.java
@@ -1,5 +1,6 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.event.MouseInputAdapter;
 
 import org.w3c.dom.events.MouseEvent;
 
@@ -34,37 +35,13 @@ public class AffichageChat extends JPanel{
         //gestion de la zone de texte pour envoyer un message et du bouton d'envoi du message à droite de la zone de texte
         JTextField messageAEnvoyer = new JTextField("Message à envoyer");
         messageAEnvoyer.setPreferredSize(new Dimension(largeur - 100, hauteur - 400));
-        messageAEnvoyer.addMouseListener(new MouseListener(){
+        messageAEnvoyer.addMouseListener(new MouseInputAdapter() {
             @Override
             public void mouseClicked(java.awt.event.MouseEvent e) {
-                // TODO Auto-generated method stub
-                messageAEnvoyer.setText("");                
+                messageAEnvoyer.setText("");
             }
-
-            @Override
-            public void mousePressed(java.awt.event.MouseEvent e) {
-                // TODO Auto-generated method stub
-                
-            }
-
-            @Override
-            public void mouseReleased(java.awt.event.MouseEvent e) {
-                // TODO Auto-generated method stub
-                
-            }
-
-            @Override
-            public void mouseEntered(java.awt.event.MouseEvent e) {
-                // TODO Auto-generated method stub
-                
-            }
-
-            @Override
-            public void mouseExited(java.awt.event.MouseEvent e) {
-                // TODO Auto-generated method stub
-                
-            }
-         });
+        });
+        
     
         //Petit trick pour avoir une image sur un bouton de la bonne taille
         ImageIcon iconeDeBonneTaille = new ImageIcon();
