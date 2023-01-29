d0617d5fa192a98eec7e4c392d8b10f76c405707
arthur <art.boullier@gmail.com>
2023-01-29 18:16:16 +0100

    victoire réparée

--- Grille.java
+++ Grille.java
@@ -16,7 +16,7 @@ public class Grille{
 
     public boolean verificationVictoire(){
         // Vérifie les lignes horizontales
-        for (int i = 0; i < 3; i += 3) {
+        for (int i = 0; i < 9; i += 3) {
             if (this.listeDeCases.get(i).identiques(this.listeDeCases.get(i + 1)) && this.listeDeCases.get(i + 1).identiques(this.listeDeCases.get(i + 2))) {
                 return true;
             }
@@ -24,21 +24,15 @@ public class Grille{
         // Vérifie les colonnes verticales
         for (int i = 0; i < 3; i++) {
             if (this.listeDeCases.get(i).identiques(this.listeDeCases.get(i + 3)) && this.listeDeCases.get(i + 3).identiques(this.listeDeCases.get(i + 6))) {     
-                System.out.println("Victoire");
-
                 return true;
             }
         }
         // Vérifie les diagonales
         if (this.listeDeCases.get(0).identiques(this.listeDeCases.get(4)) && this.listeDeCases.get(4).identiques(this.listeDeCases.get(8))) {
-            System.out.println("Victoire");
-
-            return true;
+         return true;
         }
         if (this.listeDeCases.get(2).identiques(this.listeDeCases.get(4)) && this.listeDeCases.get(4).identiques(this.listeDeCases.get(6))) {
-            System.out.println("Victoire");
-
-            return true;
+          return true;
         }
         return false;
     }
